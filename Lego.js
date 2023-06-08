29f2ecd020c5e8478e4afa4ba53a68e35f6c5b1a
Roman15122022 <mallrom1231@gmail.com>
2023-06-08 23:52:50 +0300

    feat: get/set

--- Lego.js
+++ Lego.js
@@ -12,6 +12,12 @@ class LegoMan{
         let ans = this.weight/this.height**2;
         console.log(`Your bmi: ${Math.round(ans)} `);
     }
+    set newWeight(value){
+        this.weight = value;
+    }
+    get represent(){
+        console.log(`Hello, ${this.name}! Your parametrs : ${this.weight} kg, ${this.height} m , ${this.age} years old`);
+    }
 }
 
 const roma = new LegoMan('Roma', 18, 77, 1.77);
